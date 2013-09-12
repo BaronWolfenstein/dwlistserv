a5261c92d8c16f1faaf013d912ce8149df87cca9
BaronWolfenstein <selimthegrim@gmail.com>
2013-09-11 21:19:51 -0700

    Heroku env var for secret

--- config/application.rb
+++ config/application.rb
@@ -39,6 +39,9 @@ module Listserver
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
+    #fixes vulnerability
+    config.secret_token = ENV['MYAPP_SECRET_TOKEN']
+
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
 
